73f8332150bcef6441c862564afd7986f6d2e80f
bielnog600 <bielnog600@gmail.com>
2025-04-17 16:52:26 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,40 +1,16 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
-import { vitePlugin as remixVitePlugin } from '@remix-run/dev';
-import UnoCSS from 'unocss/vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
-import { optimizeCssModules } from 'vite-plugin-optimize-css-modules';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-export default defineConfig((config) => ({
-  plugins: [
-    // 👇 O plugin do Remix deve estar no topo
-    remixVitePlugin({
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-        v3_lazyRouteDiscovery: true,
-      },
-    }),
-    UnoCSS(),
-    tsconfigPaths(),
-    nodePolyfills({
-      include: ['buffer', 'process', 'util', 'stream'],
-      globals: {
-        Buffer: true,
-        process: true,
-        global: true,
-      },
-      protocolImports: true,
-      exclude: ['child_process', 'fs', 'path'],
-    }),
-    config.mode === 'production' && optimizeCssModules({ apply: 'build' }),
-  ],
-  define: {
-    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
   },
   build: {
-    target: 'esnext',
-  },
-  envPrefix: ['VITE_'],
-}));
+    sourcemap: true,
+  }
+});
